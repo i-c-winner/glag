cc19d42f27ca91dc6aec9c4eae502b6afc1c2b78
Belousov Dmitriy <dmitriy@pop-os.localdomain>
2023-04-25 11:01:05 +0500

    Fix: Нало по другому

--- src/XMPP/XMMEvents.ts
+++
@@ -1,9 +0,0 @@
-import AbstractEvent from "../../AbstactEvent";
-
-class XMMEvent extends AbstractEvent {
-  constructor(props: any) {
-    super(props);
-  }
-}
-
-export default XMMEvent
--- src/XMPP/constants.ts
+++
@@ -1,5 +0,0 @@
-const constants= {
-  REGISTRED: 'XMPP_registration',
-  MESSAGE: 'XMPP_message'
-}
-export default constants
